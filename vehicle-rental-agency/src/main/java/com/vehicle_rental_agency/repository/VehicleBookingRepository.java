81ac47f1ba0f5f868551b2fa82f0e0505bb9586b
nivedita-indore <javaprojectsri1@gmail.com>
2024-07-17 11:42:12 +0530

    Revert "initial changes"

--- vehicle-rental-agency/src/main/java/com/vehicle_rental_agency/repository/VehicleBookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vehicle_rental_agency.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vehicle_rental_agency.entity.VehicleBooking;
-
-public interface VehicleBookingRepository extends JpaRepository<VehicleBooking, Long> {
-
-}
